94a9ca8b7735219a66ee8ffc3c9e70c5360215e5
bala <bala01.mobility@gmail.com>
2022-05-21 19:43:40 +0530

    transport_type

--- resources/views/admin/types/update.blade.php
+++ resources/views/admin/types/update.blade.php
@@ -87,28 +87,25 @@
 
         </div>
     </div>
-                <div class="col-6">
-                <div class="form-group">
-                <label for="transport_type">@lang('view_pages.tansport_type')
-                <span class="text-danger">*</span>
-                </label>
-                <select name="transport_type" id="transport_type" class="form-control"
-                required>
-                <option value="{{ $type->is_taxi }}" {{ old('is_taxi',$type->is_taxi) ? 'selected' : '' }} > 
-                    @if($type->is_taxi == 'taxi' )
-                        {{ "Taxi" }}
-                    @else
-                        {{ "Delivery" }}
-                    @endif
-                </option>
-                <option value="taxi">@lang('view_pages.taxi')</option>
-                <option value="delivery">@lang('view_pages.delivery')</option>
-                </select>
-                <span class="text-danger">{{ $errors->first('service_location_id') }}</span>
-                </div>
-                </div>
+                                    <div class="col-6">
+                                        <div class="form-group">
+                                            <label for="transport_type">@lang('view_pages.tansport_type')
+                                                <span class="text-danger">*</span>
+                                            </label>
+                                            <select name="transport_type" id="transport_type" class="form-control" required>
+                                                <option value="">@lang('view_pages.select_tansport_type')</option>
+                                                <option value='male'
+                                                    {{ old('transport_type', $type->is_taxi) == 'taxi' ? 'selected' : '' }}>
+                                                    @lang('view_pages.taxi')</option>
+                                                <option value='fe-male'
+                                                    {{ old('transport_type', $type->is_taxi) == 'fe-male' ? 'selected' : '' }}>
+                                                    @lang('view_pages.delivery')</option>
+                                            </select>
+                                            <span class="text-danger">{{ $errors->first('transport_type') }}</span>
 
+                                        </div>
 
+                                    </div>
 </div>
 
     <div class="form-group">
