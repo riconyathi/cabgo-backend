6a077c4a462c09c6ec886ac5bcaaa9340f0508a6
Dilip <tagyourtaxi@gmail.com>
2022-05-21 23:25:23 +0530

    local changes

--- app/Console/Commands/OfflineUnAvailableDrivers.php
+++ app/Console/Commands/OfflineUnAvailableDrivers.php
@@ -50,6 +50,8 @@ class OfflineUnAvailableDrivers extends Command
         foreach ($drivers as $key => $driver) {
             $driver_updated_at = Carbon::createFromTimestamp($driver['updated_at']);
 
+            dd($driver['updated_at']);
+            
             if ($conditional_timestamp > $driver['updated_at']) {
 
                 // $updatable_offline_date_time = Carbon::createFromTimestamp($driver['updated_at']);
