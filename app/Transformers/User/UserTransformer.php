2cf14780175e5b4edf070cace2d8d955eda0b679
Dilip <tagyourtaxi@gmail.com>
2022-07-06 13:35:59 +0530

    settings updates

--- app/Transformers/User/UserTransformer.php
+++ app/Transformers/User/UserTransformer.php
@@ -79,7 +79,7 @@ class UserTransformer extends Transformer
 
         $params['user_can_make_a_ride_after_x_miniutes'] = get_settings(Settings::USER_CAN_MAKE_A_RIDE_AFTER_X_MINIUTES);
         
-        $params['maximum_time_for_find_drivers_for_regular_ride'] = get_settings(Settings::MAXIMUM_TIME_FOR_FIND_DRIVERS_FOR_REGULAR_RIDE);
+        $params['maximum_time_for_find_drivers_for_regular_ride'] = (get_settings(Settings::MAXIMUM_TIME_FOR_FIND_DRIVERS_FOR_REGULAR_RIDE) *60);
 
         return $params;
     }
