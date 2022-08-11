ecfffabc7b94ef4987729cf7ed58092b9c590b53
Dilip <tagyourtaxi@gmail.com>
2022-08-11 18:02:00 +0530

    owner earnings updates

--- app/Models/Payment/OwnerWalletHistory.php
+++ app/Models/Payment/OwnerWalletHistory.php
@@ -15,4 +15,36 @@ class OwnerWalletHistory extends Model
     {
         return $this->belongsTo(Owner::class, 'user_id', 'id');
     }
+
+
+    /**
+    * Get formated and converted timezone of user's created at.
+    *
+    * @param string $value
+    * @return string
+    */
+    public function getConvertedCreatedAtAttribute()
+    {
+        if ($this->created_at==null||!auth()->user()->exists()) {
+            return null;
+        }
+        $timezone = auth()->user()->timezone?:env('SYSTEM_DEFAULT_TIMEZONE');
+        return Carbon::parse($this->created_at)->setTimezone($timezone)->format('jS M h:i A');
+    }
+    /**
+    * Get formated and converted timezone of user's created at.
+    *
+    * @param string $value
+    * @return string
+    */
+    public function getConvertedUpdatedAtAttribute()
+    {
+        if ($this->updated_at==null||!auth()->user()->exists()) {
+            return null;
+        }
+        $timezone = auth()->user()->timezone?:env('SYSTEM_DEFAULT_TIMEZONE');
+        return Carbon::parse($this->updated_at)->setTimezone($timezone)->format('jS M h:i A');
+    }
+
+
 }
