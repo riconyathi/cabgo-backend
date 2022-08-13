2dfb3b9bb36b93cdbd976cdada63344d335927fd
Dilip <tagyourtaxi@gmail.com>
2022-08-13 13:10:16 +0530

    firebae updateS

--- app/Console/Commands/AssignDriversForRegularRides.php
+++ app/Console/Commands/AssignDriversForRegularRides.php
@@ -56,7 +56,7 @@ class AssignDriversForRegularRides extends Command
     public function handle()
     {
         $current_time = Carbon::now()->format('Y-m-d H:i:s');
-        $sub_5_min = Carbon::now()->subMinutes(7)->format('Y-m-d H:i:s');
+        $sub_5_min = Carbon::now()->subMinutes(20)->format('Y-m-d H:i:s');
         // DB::enableQueryLog();
         $requests = Request::where('is_later', 0)
                     ->where('created_at', '<=', $current_time)
